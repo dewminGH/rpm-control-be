a51ca6c1d1a42ade633e65e9cfef563fe29e605b
dewminGH <trashancsgo@gmail.com>
2025-03-30 14:26:10 +0530

    allow headers

--- python_fn/settings.py
+++ python_fn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@*vn$tg0mj0%n5dgmhu^fjx(_s#=vvubgcx-gca8bvyhycb8d$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cat-booster-x.onrender.com', 'yourdomain.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
