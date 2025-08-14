4a18b593c63aa44f2a4bf20c2d3a1e69bb981853
Tharunda Dewmin <tharunda@creatit.com.au>
2025-08-14 20:10:25 +0530

    sql sample

--- .base_script.sql
+++ .base_script.sql
@@ -1,16 +1,16 @@
-
 CREATE TABLE Users (
     user_id serial PRIMARY KEY,
     user_name VARCHAR(255),
-    device_secret VARCHAR(255),
-    constraint c1 foreign key (device_secret)
-references fan_rpms(device_secret)
+    rpm_id int,
+    constraint c1 foreign key (rpm_id)
+references fan_rpms(id)
 );
 
 drop table Users
 
 create table fan_rpms(
-device_secret varchar (255) PRIMARY KEY,
+id serial PRIMARY KEY,
+device_secret VARCHAR(255),
 fan_speed float,
 time date,
 temperature float,
@@ -25,9 +25,10 @@ drop table fan_rpms
 
 insert into Users (user_name) values ('test-1')
 
-select * from fan_rpms
+select * from fanRpms
 
-insert into fan_rpms (device_secret,
+insert into fan_rpms (
+device_secret,
 fan_speed ,
 time ,
 temperature ,
@@ -35,3 +36,4 @@ humidity ,
 light ,
 cO2,
 container_type) values ('ds-2',1400 ,'2025-08-13',30,1300,1300,1300,'apple') 
+
